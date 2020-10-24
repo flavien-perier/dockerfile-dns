2372508ae7d4bae1465b8487fb9460a1d827e1f8
Flavien PERIER <flavien.perier@pm.me>
2020-10-24 14:51:41 +0200

    Correct db.local

--- start.sh
+++ start.sh
@@ -2,18 +2,19 @@
 
 FIRST_DOMAIN=`echo $SUB_DOMAINS | cut -f 1 -d " "`
 
-SERVER_IP_INT=`ping $SERVER_DOMAIN -c 1 | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d ")"`
-BASE_PATH_INT=`echo $FIRST_DOMAIN.$BASE_PATH`
+SERVER_IP=`ping $SERVER_DOMAIN -c 1 | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d ")"`
+BASE_DOMAIN=`echo $FIRST_DOMAIN.$BASE_PATH`
 
 DNS_LIST=`echo $SUB_DNS | tr " " ";" | sed '$s/$/;/'`
 
 cat /root/named.conf | \
-    sed s/"@DNS_LIST"/"$DNS_LIST"/ > /etc/bind/named.conf
+    sed s/"@DNS_LIST"/"$DNS_LIST"/g > /etc/bind/named.conf
 
 cat /root/db.local | \
-    sed s/"@SERVER_IP"/"$SERVER_IP_INT"/ | \
-    sed s/"@FIRST_DOMAIN"/"$FIRST_DOMAIN"/ | \
-    sed s/"@BASE_PATH"/"$BASE_PATH_INT"/ > /etc/bind/db.local
+    sed s/"@SERVER_IP"/"$SERVER_IP"/g | \
+    sed s/"@FIRST_DOMAIN"/"$FIRST_DOMAIN"/g | \
+    sed s/"@BASE_PATH"/"$BASE_PATH"/g | \
+    sed s/"@BASE_DOMAIN"/"$BASE_DOMAIN"/g > /etc/bind/db.local
 
 for DOMAIN in `echo $SUB_DOMAINS | cut -f 2- -d " "`
 do

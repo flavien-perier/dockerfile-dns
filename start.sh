0cacd9e0b3774a47436da25c3f80ff924ec026b2
Flavien PERIER <flavien.perier@pm.me>
2021-06-04 17:55:22 +0200

    Docker cron

--- start.sh
+++ start.sh
@@ -5,16 +5,16 @@ FIRST_DOMAIN=`echo $SUB_DOMAINS | cut -f 1 -d " "`
 SERVER_IP=`ping $SERVER_DOMAIN -c 1 | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d ")"`
 BASE_DOMAIN=`echo $FIRST_DOMAIN.$BASE_PATH`
 
-DNS_LIST=`echo $SUB_DNS | tr " " ";" | sed '$s/$/;/'`
+DNS_LIST=`echo $SUB_DNS | sed -e 's/ /;/g' -e '$s/$/;/'`
 
 cat /root/named.conf | \
     sed s/"@DNS_LIST"/"$DNS_LIST"/g > /etc/bind/named.conf
 
-cat /root/db.local | \
-    sed s/"@SERVER_IP"/"$SERVER_IP"/g | \
-    sed s/"@FIRST_DOMAIN"/"$FIRST_DOMAIN"/g | \
-    sed s/"@BASE_PATH"/"$BASE_PATH"/g | \
-    sed s/"@BASE_DOMAIN"/"$BASE_DOMAIN"/g > /etc/bind/db.local
+cat /root/db.local | sed \
+    -e s/"@SERVER_IP"/"$SERVER_IP"/g \
+    -e s/"@FIRST_DOMAIN"/"$FIRST_DOMAIN"/g \
+    -e s/"@BASE_PATH"/"$BASE_PATH"/g \
+    -e s/"@BASE_DOMAIN"/"$BASE_DOMAIN"/g > /etc/bind/db.local
 
 for DOMAIN in `echo $SUB_DOMAINS | cut -f 2- -d " "`
 do
